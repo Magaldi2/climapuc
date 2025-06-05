799caaa1cbeb694dad2462162de7847d786029d0
Guilherme Andrade <guiandradeemail@gmail.com>
2025-06-05 16:51:08 -0300

    🐛 fix: structure

--- dockerfile
+++ dockerfile
@@ -22,4 +22,4 @@ RUN chmod +x /monitor.sh
 
 COPY . .
 
-CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:80", "app:app"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:80", "src.app:app"]
--- src/app.py
+++ src/app.py
@@ -11,10 +11,15 @@ import jwt
 import os
 from datetime import datetime, timedelta , timezone
 import json
+# from auth import validar_api_login
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder='./public/templates',
+    static_folder='./public/static'
+)
 
 # Configs de login da pagina da constanta
 API_URL = os.getenv("API_URL")
@@ -36,7 +41,7 @@ def validar_api_login():
         return True
     
     # Verifica se tem salvo
-    with open('auth.json', 'r') as file:
+    with open('src/auth.json', 'r') as file:
         data = json.load(file)
         print("Leu json")
         if data['token'] and data['expiresIn'] and (data['expiresIn'] > (time.time() + 60)):
@@ -76,7 +81,7 @@ def validar_api_login():
             print(f"Token (início): {api_auth_token_full[:15]}...")
             print(f"Expira em: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time))}")
 
-            with open('auth.json', 'w') as file:
+            with open('src/auth.json', 'w') as file:
                 data = {
                     "expiresIn": token_expiration_time,
                     "token": api_auth_token_full,
@@ -255,219 +260,219 @@ def get_modems():
         print(f"Erro ao buscar modems: {e}")
         return jsonify({'error': 'Erro ao buscar modems'}), 500
 
-@app.route('/')
-def index():
-    current_data, previous_data = get_mysql_data()
-    if not current_data:
-        return render_template(
-            'index.html', 
-            message="Nenhum dado disponível no momento.",
-            wind_direction="N/D",
-            wind_icon_class="rotate-0",
-            uv_status="N/A",
-            humidity_status="N/A",
-            rain_status="N/A",
-            temperature_status="N/A",
-            temperature=0,
-            uv_index=0,
-            humidity=0,
-            rain_level=0,
-            average_wind_speed=0,  # Adicionando a variável average_wind_speed com valor padrão
-            wind_speed_status="N/A",  # Adicionando a variável wind_speed_status com valor padrão
-            wind_speed_kmh=0  # Adicionando a variável wind_speed_kmh com valor padrão
-        )
+# @app.route('/')
+# def index():
+#     current_data, previous_data = get_mysql_data()
+#     if not current_data:
+#         return render_template(
+#             'index.html', 
+#             message="Nenhum dado disponível no momento.",
+#             wind_direction="N/D",
+#             wind_icon_class="rotate-0",
+#             uv_status="N/A",
+#             humidity_status="N/A",
+#             rain_status="N/A",
+#             temperature_status="N/A",
+#             temperature=0,
+#             uv_index=0,
+#             humidity=0,
+#             rain_level=0,
+#             average_wind_speed=0,  # Adicionando a variável average_wind_speed com valor padrão
+#             wind_speed_status="N/A",  # Adicionando a variável wind_speed_status com valor padrão
+#             wind_speed_kmh=0  # Adicionando a variável wind_speed_kmh com valor padrão
+#         )
 
-    # Dados dos sensores atuais
-    wind_direction_rad = float(current_data.get('wind_direction', 0))
-    uv_index = float(current_data.get('uv_index', 0))
-    humidity = float(current_data.get('humidity', 0))
-    current_rain_level = float(current_data.get('rain_level', 0))
-    temperature = float(current_data.get('temperature', 0))
-    average_wind_speed = float(current_data.get('average_wind_speed', 0))  # Obtendo a velocidade média do vento
+#     # Dados dos sensores atuais
+#     wind_direction_rad = float(current_data.get('wind_direction', 0))
+#     uv_index = float(current_data.get('uv_index', 0))
+#     humidity = float(current_data.get('humidity', 0))
+#     current_rain_level = float(current_data.get('rain_level', 0))
+#     temperature = float(current_data.get('temperature', 0))
+#     average_wind_speed = float(current_data.get('average_wind_speed', 0))  # Obtendo a velocidade média do vento
 
-    previous_rain_level = float(previous_data.get('rain_level', 0)) if previous_data else current_rain_level
+#     previous_rain_level = float(previous_data.get('rain_level', 0)) if previous_data else current_rain_level
 
-    # Converter radianos para direção cardeal e ícone
-    wind_direction, wind_icon_class = rad_to_direction_with_icon(wind_direction_rad)
+#     # Converter radianos para direção cardeal e ícone
+#     wind_direction, wind_icon_class = rad_to_direction_with_icon(wind_direction_rad)
 
-    # Mensagens baseadas nos valores
-    rain_status = get_rain_status(current_rain_level, previous_rain_level)
-    wind_speed_status = get_wind_speed_status(average_wind_speed)  # Obtendo o status da velocidade do vento
+#     # Mensagens baseadas nos valores
+#     rain_status = get_rain_status(current_rain_level, previous_rain_level)
+#     wind_speed_status = get_wind_speed_status(average_wind_speed)  # Obtendo o status da velocidade do vento
 
-    # Converter a velocidade do vento de m/s para km/h
-    average_wind_speed_kmh = average_wind_speed * 3.6
+#     # Converter a velocidade do vento de m/s para km/h
+#     average_wind_speed_kmh = average_wind_speed * 3.6
 
-    return render_template(
-        'index.html',
-        wind_direction=wind_direction,
-        wind_icon_class=wind_icon_class,
-        uv_status=get_uv_status(uv_index),
-        humidity_status=get_humidity_status(humidity),
-        rain_status=rain_status,
-        temperature_status=get_temperature_status(temperature),
-        temperature=temperature,
-        uv_index=uv_index,
-        humidity=humidity,
-        rain_level=current_rain_level,
-        average_wind_speed=average_wind_speed_kmh,  # Passando a variável convertida para o template
-        wind_speed_status=wind_speed_status,  # Passando a variável wind_speed_status para o template
-        wind_speed_kmh=average_wind_speed_kmh  # Passando a variável wind_speed_kmh para o template
-    )
+#     return render_template(
+#         'index.html',
+#         wind_direction=wind_direction,
+#         wind_icon_class=wind_icon_class,
+#         uv_status=get_uv_status(uv_index),
+#         humidity_status=get_humidity_status(humidity),
+#         rain_status=rain_status,
+#         temperature_status=get_temperature_status(temperature),
+#         temperature=temperature,
+#         uv_index=uv_index,
+#         humidity=humidity,
+#         rain_level=current_rain_level,
+#         average_wind_speed=average_wind_speed_kmh,  # Passando a variável convertida para o template
+#         wind_speed_status=wind_speed_status,  # Passando a variável wind_speed_status para o template
+#         wind_speed_kmh=average_wind_speed_kmh  # Passando a variável wind_speed_kmh para o template
+#     )
 
 
 
-@app.route('/dados', methods=['GET'])
-def dashboard():
-    """Rota principal para exibir o dashboard com gráficos interativos."""
-    try:
-        # Conexão com o MySQL
-        connection = mysql.connector.connect(
-            host="mysql",  # Nome do serviço MySQL no Docker Compose
-            user="root",
-            password="example",
-            database="weather_data"
-        )
-        cursor = connection.cursor(dictionary=True)
+# @app.route('/dados', methods=['GET'])
+# def dashboard():
+#     """Rota principal para exibir o dashboard com gráficos interativos."""
+#     try:
+#         # Conexão com o MySQL
+#         connection = mysql.connector.connect(
+#             host="mysql",  # Nome do serviço MySQL no Docker Compose
+#             user="root",
+#             password="example",
+#             database="weather_data"
+#         )
+#         cursor = connection.cursor(dictionary=True)
         
-        # Calcular o início e fim do dia UTC-3
-        now = datetime.now(timezone.utc)
-        local_now = now - timedelta(hours=3) # Ajuste para UTC-3
-        start_of_day = datetime(local_now.year, local_now.month, local_now.day, tzinfo=timezone.utc)
-        start_timestamp = int(start_of_day.timestamp()) + 3 * 3600
-        end_timestamp = int((start_of_day + timedelta(days=1)).timestamp()) + 3 * 3600
+#         # Calcular o início e fim do dia UTC-3
+#         now = datetime.now(timezone.utc)
+#         local_now = now - timedelta(hours=3) # Ajuste para UTC-3
+#         start_of_day = datetime(local_now.year, local_now.month, local_now.day, tzinfo=timezone.utc)
+#         start_timestamp = int(start_of_day.timestamp()) + 3 * 3600
+#         end_timestamp = int((start_of_day + timedelta(days=1)).timestamp()) + 3 * 3600
         
-        # Buscar apenas os dados do dia atual ajustado para UTC-3
-        query = """
-            SELECT timestamp, temperature, humidity, rain_level, average_wind_speed
-            FROM sensor_data 
-            WHERE temperature IS NOT NULL 
-            AND timestamp BETWEEN %s AND %s
-            ORDER BY timestamp
-        """
-        cursor.execute(query, (start_timestamp, end_timestamp))
-        data = cursor.fetchall()
-    except mysql.connector.Error as e:
-        print(f"Erro ao buscar dados do MySQL: {e}")
-        return jsonify({"message": "Erro ao buscar dados do MySQL"}), 500
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
+#         # Buscar apenas os dados do dia atual ajustado para UTC-3
+#         query = """
+#             SELECT timestamp, temperature, humidity, rain_level, average_wind_speed
+#             FROM sensor_data 
+#             WHERE temperature IS NOT NULL 
+#             AND timestamp BETWEEN %s AND %s
+#             ORDER BY timestamp
+#         """
+#         cursor.execute(query, (start_timestamp, end_timestamp))
+#         data = cursor.fetchall()
+#     except mysql.connector.Error as e:
+#         print(f"Erro ao buscar dados do MySQL: {e}")
+#         return jsonify({"message": "Erro ao buscar dados do MySQL"}), 500
+#     finally:
+#         if connection.is_connected():
+#             cursor.close()
+#             connection.close()
 
-    if not data:
-        return render_template('dashboard.html', message="Nenhum dado disponível no momento.")
+#     if not data:
+#         return render_template('dashboard.html', message="Nenhum dado disponível no momento.")
 
-    sensor_data = {"temperature": [], "humidity": [], "rain_level": [], "wind_speed": [],"timestamps": []}
-    for row in data:
-        timestamp = row["timestamp"]
-        if timestamp:
-            formatted_time = time.strftime('%H:%M', time.localtime(timestamp - 3 * 3600))
-            sensor_data["timestamps"].append(formatted_time)
-        sensor_data["temperature"].append(row["temperature"])
-        sensor_data["humidity"].append(row["humidity"])
-        sensor_data["rain_level"].append(row["rain_level"])
-        sensor_data["wind_speed"].append(row["average_wind_speed"] * 3.6)  # Adicionando a velocidade média do vento
+#     sensor_data = {"temperature": [], "humidity": [], "rain_level": [], "wind_speed": [],"timestamps": []}
+#     for row in data:
+#         timestamp = row["timestamp"]
+#         if timestamp:
+#             formatted_time = time.strftime('%H:%M', time.localtime(timestamp - 3 * 3600))
+#             sensor_data["timestamps"].append(formatted_time)
+#         sensor_data["temperature"].append(row["temperature"])
+#         sensor_data["humidity"].append(row["humidity"])
+#         sensor_data["rain_level"].append(row["rain_level"])
+#         sensor_data["wind_speed"].append(row["average_wind_speed"] * 3.6)  # Adicionando a velocidade média do vento
 
-    return render_template('dashboard.html', sensor_data=sensor_data)
+#     return render_template('dashboard.html', sensor_data=sensor_data)
 
-@app.route('/test_api_login')
-def test_api_login_route():
-    print("Rota /test_api_login foi chamada.")
-    success = validar_api_login() # Chama sua função de login
-    if success:
-        return jsonify({
-            "message": "Teste de login na API externa: SUCESSO!",
-            "token_obtido": bool(api_auth_token_full),
-            "token_preview": api_auth_token_full[:25] + "..." if api_auth_token_full else "Nenhum token",
-            "expira_em": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time)) if token_expiration_time else "N/A"
-        })
-    else:
-        return jsonify({
-            "message": "Teste de login na API externa: FALHOU!",
-            "token_obtido": False
-        }), 500
+# @app.route('/test_api_login')
+# def test_api_login_route():
+#     print("Rota /test_api_login foi chamada.")
+#     success = validar_api_login() # Chama sua função de login
+#     if success:
+#         return jsonify({
+#             "message": "Teste de login na API externa: SUCESSO!",
+#             "token_obtido": bool(api_auth_token_full),
+#             "token_preview": api_auth_token_full[:25] + "..." if api_auth_token_full else "Nenhum token",
+#             "expira_em": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time)) if token_expiration_time else "N/A"
+#         })
+#     else:
+#         return jsonify({
+#             "message": "Teste de login na API externa: FALHOU!",
+#             "token_obtido": False
+#         }), 500
     
 #@app.route('api/setdimmer', methods = ['GET'])
 #def dimmer():
 
 
 # Rota para gerar gráfico
-@app.route('/temperatura', methods=['GET'])
-def plot_data():
-    try:
-        # Conexão com o MySQL
-        connection = mysql.connector.connect(
-            host="mysql",  # Nome do serviço MySQL no Docker Compose
-            user="root",
-            password="example",
-            database="weather_data"
-        )
-        cursor = connection.cursor(dictionary=True)
+# @app.route('/temperatura', methods=['GET'])
+# def plot_data():
+#     try:
+#         # Conexão com o MySQL
+#         connection = mysql.connector.connect(
+#             host="mysql",  # Nome do serviço MySQL no Docker Compose
+#             user="root",
+#             password="example",
+#             database="weather_data"
+#         )
+#         cursor = connection.cursor(dictionary=True)
         
-        # Calcular o início e fim do dia UTC-3
-        now = datetime.now(timezone.utc)
-        local_now = now - timedelta(hours=3) # Ajuste para UTC-3
-        start_of_day = datetime(local_now.year, local_now.month, local_now.day, )
-        start_timestamp = int(start_of_day.timestamp()) + 3 * 3600
-        end_timestamp = int((start_of_day + timedelta(days=1)).timestamp()) + 3 * 3600
+#         # Calcular o início e fim do dia UTC-3
+#         now = datetime.now(timezone.utc)
+#         local_now = now - timedelta(hours=3) # Ajuste para UTC-3
+#         start_of_day = datetime(local_now.year, local_now.month, local_now.day, )
+#         start_timestamp = int(start_of_day.timestamp()) + 3 * 3600
+#         end_timestamp = int((start_of_day + timedelta(days=1)).timestamp()) + 3 * 3600
 
         
-        # Buscar apenas os dados do dia atual ajustado para UTC-3
-        query = """
-            SELECT * FROM sensor_data 
-            WHERE temperature IS NOT NULL 
-            AND timestamp BETWEEN %s AND %s
-            ORDER BY timestamp
-        """
-        cursor.execute(query, (start_timestamp, end_timestamp))
-        data = cursor.fetchall()
+#         # Buscar apenas os dados do dia atual ajustado para UTC-3
+#         query = """
+#             SELECT * FROM sensor_data 
+#             WHERE temperature IS NOT NULL 
+#             AND timestamp BETWEEN %s AND %s
+#             ORDER BY timestamp
+#         """
+#         cursor.execute(query, (start_timestamp, end_timestamp))
+#         data = cursor.fetchall()
         
-    except mysql.connector.Error as e:
-        print(f"Erro ao buscar dados do MySQL: {e}")
-        return jsonify({"message": "Erro ao buscar dados do MySQL"}), 500
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
+#     except mysql.connector.Error as e:
+#         print(f"Erro ao buscar dados do MySQL: {e}")
+#         return jsonify({"message": "Erro ao buscar dados do MySQL"}), 500
+#     finally:
+#         if connection.is_connected():
+#             cursor.close()
+#             connection.close()
 
-    if not data:
-        return jsonify({"message": "Nenhum dado disponível para o dia atual"}), 404
+#     if not data:
+#         return jsonify({"message": "Nenhum dado disponível para o dia atual"}), 404
 
-    # Extrair timestamps e valores de temperatura
-    timestamps, values = [], []
-    for row in data:
-        timestamp = row.get('timestamp')
-        temperature = row.get('temperature')
-        if timestamp and temperature is not None:
-            # Ajustar horário para UTC-3
-            adjusted_time = time.strftime('%H:%M', time.localtime(timestamp - 3 * 3600))
-            timestamps.append(adjusted_time)
-            values.append(float(temperature))
+#     # Extrair timestamps e valores de temperatura
+#     timestamps, values = [], []
+#     for row in data:
+#         timestamp = row.get('timestamp')
+#         temperature = row.get('temperature')
+#         if timestamp and temperature is not None:
+#             # Ajustar horário para UTC-3
+#             adjusted_time = time.strftime('%H:%M', time.localtime(timestamp - 3 * 3600))
+#             timestamps.append(adjusted_time)
+#             values.append(float(temperature))
 
-    if not values:
-        return jsonify({"message": "Nenhum dado de temperatura disponível para o dia atual"}), 404
+#     if not values:
+#         return jsonify({"message": "Nenhum dado de temperatura disponível para o dia atual"}), 404
 
-    # Calcular métricas (última, média, máxima e mínima)
-    last_temperature = values[-1]
-    average_temperature = sum(values) / len(values)
-    max_temperature = max(values)
-    min_temperature = min(values)
+#     # Calcular métricas (última, média, máxima e mínima)
+#     last_temperature = values[-1]
+#     average_temperature = sum(values) / len(values)
+#     max_temperature = max(values)
+#     min_temperature = min(values)
 
-    sensor_data = {"temperature": [], "timestamps":[]}
-    for row in data:
-        timestamp = row["timestamp"]
-        if timestamp:
-            formatted_time = time.strftime('%H:%M', time.localtime(timestamp - 3 * 3600))
-            sensor_data["timestamps"].append(formatted_time)
-        sensor_data["temperature"].append(row["temperature"])
+#     sensor_data = {"temperature": [], "timestamps":[]}
+#     for row in data:
+#         timestamp = row["timestamp"]
+#         if timestamp:
+#             formatted_time = time.strftime('%H:%M', time.localtime(timestamp - 3 * 3600))
+#             sensor_data["timestamps"].append(formatted_time)
+#         sensor_data["temperature"].append(row["temperature"])
 
-    # Retornar a imagem e dados para o frontend
-    return render_template('temp.html',
-                           sensor_data = sensor_data,
-                           last_temperature=last_temperature,
-                           average_temperature=average_temperature,
-                           max_temperature=max_temperature,
-                           min_temperature=min_temperature,)
+#     # Retornar a imagem e dados para o frontend
+#     # return render_template('temp.html',
+#     #                        sensor_data = sensor_data,
+#     #                        last_temperature=last_temperature,
+#     #                        average_temperature=average_temperature,
+#     #                        max_temperature=max_temperature,
+#     #                        min_temperature=min_temperature,)
 
 @app.route('/luminaires')
 def luminaires():
