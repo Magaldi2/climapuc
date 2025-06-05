a9ba8f168482d90e856db96b9b3f13c8efae024e
Guilherme Andrade <guiandradeemail@gmail.com>
2025-06-05 15:54:28 -0300

    🐛 fix: validar api

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 import jwt  
 import os
 from datetime import datetime, timedelta , timezone
+import json
 
 load_dotenv()
 
@@ -29,9 +30,20 @@ token_expiration_time = 0
 def validar_api_login():
     global api_auth_token_full, token_expiration_time
 
+    # Verifica se tem salvo
     if api_auth_token_full and (token_expiration_time > (time.time() + 60)):
-        print(f"Usando token existente. Válido até: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time))}")
+        print(f"Usando token existente (GLOBAL). Válido até: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time))}")
         return True
+    
+    # Verifica se tem salvo
+    with open('auth.json', 'r') as file:
+        data = json.load(file)
+        print("Leu json")
+        if data['token'] and data['expiresIn'] and (data['expiresIn'] > (time.time() + 60)):
+            api_auth_token_full = data['token']
+            token_expiration_time = data['expiresIn']
+            print(f"Usando token existente (JSON). Válido até: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time))}")
+            return True
 
     print(f"Token nao encontrado ou expirado. Realizando login em {API_URL}")
     login_url = f"{API_URL}/login"
@@ -63,6 +75,14 @@ def validar_api_login():
             print(f"Login na API externa bem-sucedido.")
             print(f"Token (início): {api_auth_token_full[:15]}...")
             print(f"Expira em: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(token_expiration_time))}")
+
+            with open('auth.json', 'w') as file:
+                data = {
+                    "expiresIn": token_expiration_time,
+                    "token": api_auth_token_full,
+                }
+                json.dump(data, file, indent=4, ensure_ascii=False)
+
             return True
         else:
             print("ERRO: Token ou expiração não encontrado no JWT.")
@@ -484,6 +504,13 @@ def luminaires():
 
 @app.route('/api/state', methods=['POST'])
 def on_off_luminaire():
+    auth_success = validar_api_login() # Chama sua função de login
+    if not auth_success:
+        return jsonify({
+            "message": "Teste de login na API externa: FALHOU!",
+            "token_obtido": False
+        }), 500
+    
     lamp_serial = flask_request.json.get('lampSerial')
     dimmer_value = flask_request.json.get('dimmerValue')
 
@@ -492,8 +519,11 @@ def on_off_luminaire():
     if dimmer_value is None:
         return jsonify({"error": "dimmerValue não informado"}), 400
     
+    print("auth token", api_auth_token_full)
+    if api_auth_token_full is None:
+        return jsonify({"error": "Sem auth token"}), 500
+
     headers = {"Authorization": api_auth_token_full}
-    
     url_luminaria = f"{API_URL}/luminaria/{lamp_serial}"
     try:
         response = requests.get(url_luminaria, headers=headers, timeout=10) 
@@ -507,13 +537,17 @@ def on_off_luminaire():
 
         modems = [modem["devEui"] for modem in lista_de_modems]
         payload = {
-            "commandRequest": {},
             "identifiers": modems
         }
         print(payload)
         try:
             response = requests.post(url, json=payload, headers=headers, timeout=10)
             response.raise_for_status()
+
+            url_force = f"{API_URL}/comando/setforcerelay?lampSerial={lamp_serial}&forceRelay=1"
+            response_force = requests.post(url_force, json=payload, headers=headers, timeout=10)
+            response_force.raise_for_status()
+            
             return jsonify({"message": "Comando enviado com sucesso!"})
         except Exception as e:
             return jsonify({"error": str(e)}), 500
